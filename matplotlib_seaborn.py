0394643a61ce4006b87ba48350b61d3f120ce056
Mohsen Cheraghi <38126744+mcheraghi@users.noreply.github.com>
2021-08-24 12:30:20 +0200

    Update matplotlib_seaborn.py

--- matplotlib_seaborn.py
+++ matplotlib_seaborn.py
@@ -1,1 +1,17 @@
 
+#--------------------------------------------------------------------------------Very basic plot
+fig,ax = plt.subplots()
+ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-PRCP-NORMAL"],marker = 'o',color = 'b',linestyle = '--')
+ax.plot(austin_weather["MONTH"], austin_weather["MLY-PRCP-NORMAL"])
+
+# Customize the x-axis label
+ax.set_xlabel("Time (months)")
+
+# Customize the y-axis label
+ax.set_ylabel("Precipitation (inches)")
+
+# Add the title
+ax.set_title("Weather patterns in Austin and Seattle")
+
+# Display the figure
+plt.show()
